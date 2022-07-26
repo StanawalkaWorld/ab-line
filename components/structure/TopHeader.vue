f0868878f7f69e4579348fe651ea7481946ea3b0
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-26 12:35:49 +0200

    Created navlinks.

--- components/structure/TopHeader.vue
+++ components/structure/TopHeader.vue
@@ -1,19 +1,60 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+interface Navlink {
+    name: string;
+    to: string;
+    icon?: string;
+}
+
 export default defineComponent({
     name: "top-header",
+    setup() {
+        const navlinks: Navlink[] = [
+            {
+                name: "Wycena",
+                to: "/pricing",
+                icon: "bi-tag-fill",
+            },
+            {
+                name: "Kontakt",
+                to: "/contact",
+                icon: "bi-telephone-fill",
+            },
+        ];
+
+        return {
+            navlinks,
+        };
+    },
 });
 </script>
 
 <template>
-    <header class="container mx-auto">
-        <NavLink to="/">
+    <header class="container mx-auto flex flex-col md:flex-row">
+        <NuxtLink to="/">
             <img
                 src="@/assets/img/logo/full-white.svg"
                 alt="A&B Line logo"
                 class="p-1 h-20"
             />
-        </NavLink>
+        </NuxtLink>
+        <nav class="flex flex-col md:flex-row items-center ml-10 children:mr-6">
+            <NuxtLink
+                v-for="link in navlinks"
+                :key="link.name"
+                :to="link.to"
+                class="py-1 px-3 rounded-md hover:bg-primary-300"
+            >
+                <i :class="['bi', link.icon ?? '', 'text-sm', 'mr-1']"></i>
+                {{ link.name }}
+            </NuxtLink>
+        </nav>
     </header>
 </template>
+
+<style scoped>
+nav .nuxt-link-exact-active {
+    @apply text-primary-400 font-semibold bg-primary-100 border border-primary-400;
+}
+</style>
