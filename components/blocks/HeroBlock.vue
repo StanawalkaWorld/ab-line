92f15fb857ae28acbf3c492eaf8c7911f4d0b0da
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-26 20:58:18 +0200

    Added comments to the Heroblock.

--- components/blocks/HeroBlock.vue
+++ components/blocks/HeroBlock.vue
@@ -17,13 +17,15 @@ export default defineComponent({
     >
         <!-- ⬆ Main section tag with a dimmed image -->
 
-        <!--  -->
+        <!-- ⬇ Flex container centered, that manages 2 blocks inside -->
         <div
             class="flex <lg:flex-col justify-center items-center container mx-auto"
         >
+            <!-- Half-width container for a header and motivational paragraph -->
             <div class="sm:w-1/2 <lg:(text-center mb-5)">
                 <h1 class="text-6xl font-semibold mb-8">Nagłówek</h1>
                 <p>Tekst motywujący do skorzystania z usług.</p>
+                <!-- Button link into pricing page -->
                 <NuxtLink to="/pricing">
                     <UButton
                         class="mt-10 border-2 border-primary-200 text-primary-200 font-semibold"
@@ -32,19 +34,24 @@ export default defineComponent({
                     >
                 </NuxtLink>
             </div>
+            <!-- Half-width block centering contact information -->
             <div
                 class="sm:w-1/2 flex justify-center items-center <lg:text-center"
             >
+                <!-- Resizable block div, that packs all text -->
                 <div class="min-w-1/2 bg-primary-900 px-6 py-10">
                     <h2 class="text-4xl mb-5">Skontaktuj się z nami:</h2>
+                    <!-- Address tag making text italic -->
                     <address>
                         <p>
+                            <!-- E-mail -->
                             <i class="bi bi-envelope mr-2 text-lg"></i>
                             <a href="mailto:abline.office@gmail.com"
                                 >abline.office@gmail.com</a
                             >
                         </p>
                         <p>
+                            <!-- Phone number -->
                             <i class="bi bi-telephone mr-2 text-lg"></i>
                             <a href="tel:+48883766963">+48 883 766 963</a>
                         </p>
