3c164836fa5ad7520d5510b6773ffc5ca56c7277
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-26 18:31:35 +0200

    Added UIButton.

---
+++ components/UI/UButton.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        icon: {
+            required: false,
+            type: String,
+            default: "",
+        },
+        filled: {
+            required: false,
+            type: Boolean,
+            default: true,
+        },
+    },
+});
+</script>
+
+<template>
+    <button class="py-2 px-4 rounded-md hover:cursor-pointer">
+        <i v-if="icon.length > 0" :class="['bi', icon, 'mr-1']"></i>
+        <slot />
+    </button>
+</template>
