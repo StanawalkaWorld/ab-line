63f1c57418cd044ab8aa4fa7f130521a338c9f46
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-28 14:36:49 +0200

    Style and fill the footer with info.

--- components/structure/TheFooter.vue
+++ components/structure/TheFooter.vue
@@ -1,3 +1,39 @@
 <template>
-    <footer class="w-full"></footer>
+    <footer class="bg-black text-white flex <lg:(flex-col items-center) p-10">
+        <div class="lg:w-1/2 flex justify-center items-center">
+            <div class="p-5">
+                <h3 class="text-xl text-center">
+                    <i class="bi bi-building"></i> Dane firmy:
+                </h3>
+                <ul>
+                    <li>Pełna nazwa: A&B Line Andrzej Barański</li>
+                    <li>NIP: 5811803184</li>
+                    <li>REGON: 220330804</li>
+                </ul>
+            </div>
+        </div>
+        <div class="lg:w-1/2 flex justify-center items-center">
+            <div class="p-5">
+                <h3 class="text-xl text-center">
+                    <i class="bi bi-person"></i> Kontakt:
+                </h3>
+                <ul>
+                    <li>
+                        <i class="bi bi-envelope-fill mr-2"></i>
+                        <a
+                            href="mailto:abline.office@gmail.com"
+                            class="text-primary-200"
+                            >abline.office@gmail.com</a
+                        >
+                    </li>
+                    <li>
+                        <i class="bi bi-telephone-fill mr-2"></i>
+                        <a href="tel:+48883766963" class="text-primary-200"
+                            >+48 883 766 963</a
+                        >
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </template>
