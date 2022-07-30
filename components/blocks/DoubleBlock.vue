59c8f68a43875c7d220aeac8a3ed5c1bb5e28c78
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-30 13:45:08 +0200

    Added Double block component.

---
+++ components/blocks/DoubleBlock.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { useIntersectionObserver } from "@/composables/intersection";
+
+export default defineComponent({
+    name: "DoubleBlock",
+
+    setup() {
+        const trigger = ref();
+        const { isVisible } = useIntersectionObserver(trigger, {
+            threshold: 0.6,
+        });
+
+        return {
+            isVisible,
+            trigger,
+        };
+    },
+});
+</script>
+
+<template>
+    <div
+        ref="trigger"
+        class="flex <lg:flex-col items-center justify-center h-screen-sm flex-wrap"
+        :class="{ invisible: !isVisible }"
+    >
+        <div
+            class="w-full"
+            :class="{ 'fade-slide-from-bottom-anim': isVisible }"
+        >
+            <slot />
+        </div>
+        <div
+            class="w-full lg:w-1/2"
+            :class="{ 'fade-slide-from-left-anim': isVisible }"
+        >
+            <slot name="left" />
+        </div>
+        <div
+            class="w-full lg:w-1/2"
+            :class="{ 'fade-slide-from-right-anim': isVisible }"
+        >
+            <slot name="right" />
+        </div>
+    </div>
+</template>
