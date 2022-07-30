409e96b93859bdd164086b7de8e12d37fa9383a5
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-30 11:33:25 +0200

    Created Monad typings.

---
+++ types/monad.ts
@@ -0,0 +1,12 @@
+import { Ref } from "vue";
+
+export type Maybe<T> = T | undefined;
+export type MaybeRef<T> = Ref<T> | T;
+
+export function monadRun<T, R>(val: Maybe<T>, func: (val: T) => R): Maybe<R> {
+    if(val === undefined) {
+        return undefined;
+    }
+    
+    return func(val as T);
+}
