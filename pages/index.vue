41003066126423de6e4a7b0f100d778dd7dd2d3f
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-30 13:45:40 +0200

    Created a test block.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,14 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import HeroBlock from "~/components/blocks/HeroBlock.vue";
+import BeLazy from "~/components/UI/BeLazy.vue";
+import DoubleBlock from "~/components/blocks/DoubleBlock.vue";
 
 export default defineComponent({
     setup() {
         return { msg: "Hello!" };
     },
-    components: { HeroBlock },
+    components: { HeroBlock, BeLazy, DoubleBlock },
     head: {
         title: "Strona główna - A&B Line",
     },
@@ -16,5 +18,30 @@ export default defineComponent({
 <template>
     <div>
         <HeroBlock />
+
+        <div class="h-screen"></div>
+
+        <DoubleBlock>
+            <h3 class="text-6xl text-center">Lorem ipsum dolor sit amet.</h3>
+            <template #left>
+                <h3 class="text-3xl text-justify p-6">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab
+                    numquam delectus, sunt ex dignissimos officia expedita
+                    voluptate magni velit quos quam unde consequuntur pariatur
+                    aliquid voluptas nihil vero deleniti minima voluptates
+                    consequatur. Ratione vitae saepe atque voluptate amet rem
+                    commodi mollitia iusto hic maxime, blanditiis ea veritatis.
+                    Repudiandae, ipsam cumque.
+                </h3>
+            </template>
+            <template #right>
+                <div
+                    :style="{
+                        backgroundImage: `url('${require('@/assets/img/pexels-mike-b-trucking.webp')}')`,
+                    }"
+                    class="h-96 w-full bgimage bg-blend-multiply bg-true-gray-400 m-6"
+                ></div>
+            </template>
+        </DoubleBlock>
     </div>
 </template>
