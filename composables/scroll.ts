20ec59190a4c4ee275f75f1e35d62d15dc63307d
StanawalkaWorld <stanawalkaworld@gmail.com>
2022-07-30 22:28:00 +0200

    Fixed formatting. Added items folder into more ignore files.

--- composables/scroll.ts
+++ composables/scroll.ts
@@ -2,26 +2,25 @@ import { unref } from "vue";
 import { MaybeRef, monadRun } from "~/types/monad";
 
 export const useSmoothScroll = () => {
-    
     const scrollTo = (el: MaybeRef<string>) => {
-        if(process.client) {
+        if (process.client) {
             monadRun(document.querySelector(unref(el)), (element) => {
                 element.scrollIntoView({
                     behavior: "smooth",
-                    block: "center"
-                })
-            })
+                    block: "center",
+                });
+            });
         }
-    }
+    };
 
     const scrollToElement = (el: MaybeRef<Element>) => {
         unref(el).scrollIntoView({
-            behavior: "smooth"
+            behavior: "smooth",
         });
-    }
+    };
 
     return {
         scrollTo,
-        scrollToElement
-    }
-}
+        scrollToElement,
+    };
+};
