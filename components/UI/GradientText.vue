8603108212c47070e50171b1523b9dffcfdb72c4
StanawalkaWorld <stanawalkaworld@gmail.com>
2023-01-15 00:17:01 +0100

    Updated GradientText

--- components/UI/GradientText.vue
+++ components/UI/GradientText.vue
@@ -1,11 +1,3 @@
-<script>
-import { defineComponent } from "vue";
-
-export default defineComponent({
-    name: "GradientText",
-});
-</script>
-
 <template>
     <h1
         class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-primary-500"
